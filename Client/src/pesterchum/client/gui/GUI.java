52cb4e0171be23ca0261e99dd443befc0f617803
aleksandra <alb31@aber.ac.uk>
2013-01-19 21:31:51 +0000

    menu time

--- Client/src/pesterchum/client/gui/GUI.java
+++ Client/src/pesterchum/client/gui/GUI.java
@@ -4,9 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 
 import pesterchum.client.resource.Img;
 import pesterchum.client.resource.Resource;
@@ -16,13 +14,30 @@ public class GUI extends JFrame{
 	private static final long serialVersionUID = 1L;
 	private ResourceLoader smilies;
 	public GUI(){
+		JMenuBar menu;
+		JMenu client, profile, help;
+		JMenuItem options;
+		// menu at top
+		// logo
+		//list of chums
+		//buttons add chum/block[red]/pester!
+		//mychumhandle: mood/ nick/color
+		//mood table 2x6 + abscond
+		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		smilies = new ResourceLoader();
 		smilies.load("/smilies/smilies.xml");
 		this.setSize(new Dimension(200, 400));
 		this.getContentPane().setBackground(Color.ORANGE);
-		this.add(new JLabel(getIcon(smilies.getResource("apple"))), BorderLayout.CENTER);
-		this.add(new PLabel("Label", getIcon(smilies.getResource("apple"))),BorderLayout.SOUTH);
+		//create menu bar + menus
+		menu= new JMenuBar();
+		client = new JMenu("client"); profile = new JMenu("profile"); help = new JMenu("help");
+		menu.add(client); menu.add(profile); menu.add(help);
+		
+		options = new JMenuItem("options");
+		
+		//this.add(new JLabel(getIcon(smilies.getResource("apple"))), BorderLayout.CENTER);
+		//this.add(new PLabel("Label", getIcon(smilies.getResource("apple"))),BorderLayout.SOUTH);
 		this.setVisible(true);
 	}
 	private ImageIcon getIcon(Resource res){
