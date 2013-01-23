a685dc91bac0e4ce65610525c59f2662fa17d8fc
Tim Stableford <tim.stableford1@gmail.com>
2013-01-23 11:39:24 +0000

    Changed my mind, wanted to use a sqlite jdbc driver

--- PesterServer/src/pesterchum/server/data/database/SQLiteDatabase.java
+++ PesterServer/src/pesterchum/server/data/database/SQLiteDatabase.java
@@ -2,22 +2,9 @@ package pesterchum.server.data.database;
 
 import java.io.File;
 
-import org.tmatesoft.sqljet.core.SqlJetException;
-import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
-import org.tmatesoft.sqljet.core.table.ISqlJetTransaction;
-import org.tmatesoft.sqljet.core.table.SqlJetDb;
-
 public class SQLiteDatabase implements Database{
-	public SQLiteDatabase(File database) throws SqlJetException{
-		SqlJetDb db = SqlJetDb.open(database, true);
-		// set DB option that have to be set before running any transactions: 
-		db.getOptions().setAutovacuum(true);
-		// set DB option that have to be set in a transaction: 
-		db.runTransaction(new ISqlJetTransaction() {
-			public Object run(SqlJetDb db) throws SqlJetException {
-				db.getOptions().setUserVersion(1);
-				return true;
-			}
-		}, SqlJetTransactionMode.WRITE);
+	public SQLiteDatabase(File database){
+		
 	}
+		
 }
